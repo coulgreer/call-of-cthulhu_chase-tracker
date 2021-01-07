a934a99ef19ee20b7ea9b4d888f63294cd4329b9
cagreer18 <coulgreer1@hotmail.com>
2021-01-07 11:53:30 -0500

    Began implementation on statistic thresholds.

--- src/components/StatisticDisplay/StatisticDisplay.test.tsx
+++ src/components/StatisticDisplay/StatisticDisplay.test.tsx
@@ -15,3 +15,67 @@ test("renders statistics", () => {
   const statisticValue = screen.getByLabelText(title).textContent;
   expect(statisticValue).toBe(startingValue.toString());
 });
+
+test("renders statistics with an additional className when value is within lower warning threshold", () => {
+  const title = "A Title";
+  const startingValue = 6;
+  const lowerWarning = 6;
+  const lowerLimit = 5;
+
+  render(
+    <StatisticDisplay
+      title={title}
+      startingValue={startingValue}
+      lowerWarning={lowerWarning}
+      lowerLimit={lowerLimit}
+    />
+  );
+
+  expect(screen.getByText(title)).toBeInTheDocument();
+
+  const valueEl = screen.getByLabelText(title);
+  const statisticValue = valueEl.textContent;
+  expect(statisticValue).toBe(startingValue.toString());
+
+  expect(valueEl).toHaveClass(StatisticDisplay.LOWER_WARNING_CLASS);
+  expect(valueEl).not.toHaveClass(StatisticDisplay.LOWER_LIMIT_CLASS);
+});
+
+test("renders statistics without any additional className when thresholds are not provided", () => {
+  const title = "A Title";
+  const startingValue = -6;
+
+  render(<StatisticDisplay title={title} startingValue={startingValue} />);
+
+  expect(screen.getByText(title)).toBeInTheDocument();
+
+  const valueEl = screen.getByLabelText(title);
+  const statisticValue = valueEl.textContent;
+  expect(statisticValue).toBe(startingValue.toString());
+
+  expect(valueEl).not.toHaveClass(StatisticDisplay.LOWER_WARNING_CLASS);
+  expect(valueEl).not.toHaveClass(StatisticDisplay.LOWER_LIMIT_CLASS);
+});
+
+test("renders statistics with an additional className when value is at lower limit threshold", () => {
+  const title = "A Title";
+  const startingValue = 6;
+  const lowerLimit = 6;
+
+  render(
+    <StatisticDisplay
+      title={title}
+      startingValue={startingValue}
+      lowerLimit={lowerLimit}
+    />
+  );
+
+  expect(screen.getByText(title)).toBeInTheDocument();
+
+  const valueEl = screen.getByLabelText(title);
+  const statisticValue = valueEl.textContent;
+  expect(statisticValue).toBe(startingValue.toString());
+
+  expect(valueEl).not.toHaveClass(StatisticDisplay.LOWER_WARNING_CLASS);
+  expect(valueEl).toHaveClass(StatisticDisplay.LOWER_LIMIT_CLASS);
+});
--- src/components/StatisticDisplay/StatisticDisplay.tsx
+++ src/components/StatisticDisplay/StatisticDisplay.tsx
@@ -1,17 +1,33 @@
 import React from "react";
 
 import { v4 as uuidv4 } from "uuid";
+import clsx from "clsx";
 
 import { sanitize } from "../../utils/lib/sanitizer";
 
 import "./index.css";
 
+const LOWER_WARNING_CLASS = "statistic-display-lower-warning";
+const LOWER_LIMIT_CLASS = "statistic-display-lower-limit";
+
 interface StatisticDisplayProps {
   title: string;
   startingValue: number;
+  lowerWarning: number;
+  lowerLimit: number;
 }
 
 export default class StatisticDisplay extends React.Component<StatisticDisplayProps> {
+  static defaultProps = { lowerWarning: null, lowerLimit: null };
+
+  static get LOWER_WARNING_CLASS() {
+    return LOWER_WARNING_CLASS;
+  }
+
+  static get LOWER_LIMIT_CLASS() {
+    return LOWER_LIMIT_CLASS;
+  }
+
   private id: string;
 
   constructor(props: StatisticDisplayProps) {
@@ -20,13 +36,34 @@ export default class StatisticDisplay extends React.Component<StatisticDisplayPr
     this.id = uuidv4();
   }
 
+  isValueInLowerWarning() {
+    if (!this.props.lowerWarning) return false;
+
+    return (
+      this.props.startingValue <= this.props.lowerWarning &&
+      !(this.props.startingValue <= this.props.lowerLimit)
+    );
+  }
+
+  isValueInLowerLimit() {
+    if (!this.props.lowerLimit) return false;
+
+    return this.props.startingValue <= this.props.lowerLimit;
+  }
+
   render() {
     const labelID = sanitize(this.props.title) + "-" + this.id;
+    const className = clsx({
+      [LOWER_WARNING_CLASS]: this.isValueInLowerWarning(),
+      [LOWER_LIMIT_CLASS]: this.isValueInLowerLimit(),
+    });
 
     return (
       <div className="statistic-display">
         <label id={labelID}>{this.props.title}</label>
-        <p aria-labelledby={labelID}>{this.props.startingValue}</p>
+        <p aria-labelledby={labelID} className={className}>
+          {this.props.startingValue}
+        </p>
       </div>
     );
   }
