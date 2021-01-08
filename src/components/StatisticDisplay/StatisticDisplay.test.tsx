1e71d313ffda3fc2b9b92d0344dba37afb7d060c
cagreer18 <coulgreer1@hotmail.com>
2021-01-08 14:48:26 -0500

    Began boundary value analysis tests.
    Also, began refactoring tests to follow the should-when naming convention for tests.
    
    Note: think about making a factory function for ease of use and to follow DRY.

--- src/components/StatisticDisplay/StatisticDisplay.test.tsx
+++ src/components/StatisticDisplay/StatisticDisplay.test.tsx
@@ -16,66 +16,292 @@ test("renders statistics", () => {
   expect(statisticValue).toBe(startingValue.toString());
 });
 
-test("renders statistics with an additional className when value is within lower warning threshold", () => {
-  const title = "A Title";
-  const startingValue = 6;
-  const lowerWarning = 6;
-  const lowerLimit = 5;
+describe("Threshold class names", () => {
+  test("should render statistics without any additional className when thresholds are not provided", () => {
+    const title = "A Title";
+    const startingValue = 0;
 
-  render(
-    <StatisticDisplay
-      title={title}
-      startingValue={startingValue}
-      lowerWarning={lowerWarning}
-      lowerLimit={lowerLimit}
-    />
-  );
+    render(<StatisticDisplay title={title} startingValue={startingValue} />);
 
-  expect(screen.getByText(title)).toBeInTheDocument();
+    expect(screen.getByText(title)).toBeInTheDocument();
 
-  const valueEl = screen.getByLabelText(title);
-  const statisticValue = valueEl.textContent;
-  expect(statisticValue).toBe(startingValue.toString());
+    const valueEl = screen.getByLabelText(title);
+    const statisticValue = valueEl.textContent;
+    expect(statisticValue).toBe(startingValue.toString());
 
-  expect(valueEl).toHaveClass(StatisticDisplay.LOWER_WARNING_CLASS);
-  expect(valueEl).not.toHaveClass(StatisticDisplay.LOWER_LIMIT_CLASS);
-});
+    expect(valueEl).not.toHaveClass(StatisticDisplay.UPPER_LIMIT_CLASS);
+    expect(valueEl).not.toHaveClass(StatisticDisplay.UPPER_WARNING_CLASS);
+    expect(valueEl).not.toHaveClass(StatisticDisplay.LOWER_WARNING_CLASS);
+    expect(valueEl).not.toHaveClass(StatisticDisplay.LOWER_LIMIT_CLASS);
+  });
 
-test("renders statistics without any additional className when thresholds are not provided", () => {
-  const title = "A Title";
-  const startingValue = -6;
+  describe("Lower Warning threshold", () => {
+    test("should render statistics with an additional className when value is inside threshold", () => {
+      const title = "A Title";
+      const startingValue = -8;
+      const lowerWarning = -7;
+      const lowerLimit = -19;
 
-  render(<StatisticDisplay title={title} startingValue={startingValue} />);
+      render(
+        <StatisticDisplay
+          title={title}
+          startingValue={startingValue}
+          lowerWarning={lowerWarning}
+          lowerLimit={lowerLimit}
+        />
+      );
 
-  expect(screen.getByText(title)).toBeInTheDocument();
+      expect(screen.getByText(title)).toBeInTheDocument();
 
-  const valueEl = screen.getByLabelText(title);
-  const statisticValue = valueEl.textContent;
-  expect(statisticValue).toBe(startingValue.toString());
+      const valueEl = screen.getByLabelText(title);
+      const statisticValue = valueEl.textContent;
+      expect(statisticValue).toBe(startingValue.toString());
 
-  expect(valueEl).not.toHaveClass(StatisticDisplay.LOWER_WARNING_CLASS);
-  expect(valueEl).not.toHaveClass(StatisticDisplay.LOWER_LIMIT_CLASS);
-});
+      expect(valueEl).not.toHaveClass(StatisticDisplay.UPPER_LIMIT_CLASS);
+      expect(valueEl).not.toHaveClass(StatisticDisplay.UPPER_WARNING_CLASS);
+      expect(valueEl).toHaveClass(StatisticDisplay.LOWER_WARNING_CLASS);
+      expect(valueEl).not.toHaveClass(StatisticDisplay.LOWER_LIMIT_CLASS);
+    });
 
-test("renders statistics with an additional className when value is at lower limit threshold", () => {
-  const title = "A Title";
-  const startingValue = 6;
-  const lowerLimit = 6;
+    test("should render statistics with an additional className when value is at threshold", () => {
+      const title = "A Title";
+      const startingValue = -7;
+      const lowerWarning = -7;
+      const lowerLimit = -19;
 
-  render(
-    <StatisticDisplay
-      title={title}
-      startingValue={startingValue}
-      lowerLimit={lowerLimit}
-    />
-  );
+      render(
+        <StatisticDisplay
+          title={title}
+          startingValue={startingValue}
+          lowerWarning={lowerWarning}
+          lowerLimit={lowerLimit}
+        />
+      );
 
-  expect(screen.getByText(title)).toBeInTheDocument();
+      expect(screen.getByText(title)).toBeInTheDocument();
 
-  const valueEl = screen.getByLabelText(title);
-  const statisticValue = valueEl.textContent;
-  expect(statisticValue).toBe(startingValue.toString());
+      const valueEl = screen.getByLabelText(title);
+      const statisticValue = valueEl.textContent;
+      expect(statisticValue).toBe(startingValue.toString());
 
-  expect(valueEl).not.toHaveClass(StatisticDisplay.LOWER_WARNING_CLASS);
-  expect(valueEl).toHaveClass(StatisticDisplay.LOWER_LIMIT_CLASS);
+      expect(valueEl).not.toHaveClass(StatisticDisplay.UPPER_LIMIT_CLASS);
+      expect(valueEl).not.toHaveClass(StatisticDisplay.UPPER_WARNING_CLASS);
+      expect(valueEl).toHaveClass(StatisticDisplay.LOWER_WARNING_CLASS);
+      expect(valueEl).not.toHaveClass(StatisticDisplay.LOWER_LIMIT_CLASS);
+    });
+
+    test("should render statistics with no additional className when value is outside threshold", () => {
+      const title = "A Title";
+      const startingValue = -6;
+      const lowerWarning = -7;
+      const lowerLimit = -19;
+
+      render(
+        <StatisticDisplay
+          title={title}
+          startingValue={startingValue}
+          lowerWarning={lowerWarning}
+          lowerLimit={lowerLimit}
+        />
+      );
+
+      expect(screen.getByText(title)).toBeInTheDocument();
+
+      const valueEl = screen.getByLabelText(title);
+      const statisticValue = valueEl.textContent;
+      expect(statisticValue).toBe(startingValue.toString());
+
+      expect(valueEl).not.toHaveClass(StatisticDisplay.UPPER_LIMIT_CLASS);
+      expect(valueEl).not.toHaveClass(StatisticDisplay.UPPER_WARNING_CLASS);
+      expect(valueEl).not.toHaveClass(StatisticDisplay.LOWER_WARNING_CLASS);
+      expect(valueEl).not.toHaveClass(StatisticDisplay.LOWER_LIMIT_CLASS);
+    });
+  });
+
+  describe("Lower Limit threshold", () => {
+    test("should render statistics with an additional className when value is at threshold", () => {
+      const title = "A Title";
+      const startingValue = -19;
+      const lowerWarning = -7;
+      const lowerLimit = -19;
+
+      render(
+        <StatisticDisplay
+          title={title}
+          startingValue={startingValue}
+          lowerWarning={lowerWarning}
+          lowerLimit={lowerLimit}
+        />
+      );
+
+      expect(screen.getByText(title)).toBeInTheDocument();
+
+      const valueEl = screen.getByLabelText(title);
+      const statisticValue = valueEl.textContent;
+      expect(statisticValue).toBe(startingValue.toString());
+
+      expect(valueEl).not.toHaveClass(StatisticDisplay.UPPER_LIMIT_CLASS);
+      expect(valueEl).not.toHaveClass(StatisticDisplay.UPPER_WARNING_CLASS);
+      expect(valueEl).not.toHaveClass(StatisticDisplay.LOWER_WARNING_CLASS);
+      expect(valueEl).toHaveClass(StatisticDisplay.LOWER_LIMIT_CLASS);
+    });
+
+    test("should render statistics with no additional className when value is before threshold", () => {
+      const title = "A Title";
+      const startingValue = -18;
+      const lowerLimit = -19;
+
+      render(
+        <StatisticDisplay
+          title={title}
+          startingValue={startingValue}
+          lowerLimit={lowerLimit}
+        />
+      );
+
+      expect(screen.getByText(title)).toBeInTheDocument();
+
+      const valueEl = screen.getByLabelText(title);
+      const statisticValue = valueEl.textContent;
+      expect(statisticValue).toBe(startingValue.toString());
+
+      expect(valueEl).not.toHaveClass(StatisticDisplay.UPPER_LIMIT_CLASS);
+      expect(valueEl).not.toHaveClass(StatisticDisplay.UPPER_WARNING_CLASS);
+      expect(valueEl).not.toHaveClass(StatisticDisplay.LOWER_WARNING_CLASS);
+      expect(valueEl).not.toHaveClass(StatisticDisplay.LOWER_LIMIT_CLASS);
+    });
+
+    test("should throw error when value is outside threshold", () => {
+      const originalError = console.error;
+      console.error = jest.fn();
+
+      const title = "A Title";
+      const startingValue = -20;
+      const lowerLimit = -19;
+
+      expect(() => {
+        render(
+          <StatisticDisplay
+            title={title}
+            startingValue={startingValue}
+            lowerLimit={lowerLimit}
+          />
+        );
+      }).toThrowError();
+
+      console.error = originalError;
+    });
+  });
+
+  describe("Upper Warning threshold", () => {
+    test("should render statistics with an additional className when value is inside threshold", () => {
+      const title = "A Title";
+      const startingValue = 8;
+      const upperWarning = 7;
+      const upperLimit = 19;
+
+      render(
+        <StatisticDisplay
+          title={title}
+          startingValue={startingValue}
+          upperWarning={upperWarning}
+          upperLimit={upperLimit}
+        />
+      );
+
+      expect(screen.getByText(title)).toBeInTheDocument();
+
+      const valueEl = screen.getByLabelText(title);
+      const statisticValue = valueEl.textContent;
+      expect(statisticValue).toBe(startingValue.toString());
+
+      expect(valueEl).not.toHaveClass(StatisticDisplay.UPPER_LIMIT_CLASS);
+      expect(valueEl).toHaveClass(StatisticDisplay.UPPER_WARNING_CLASS);
+      expect(valueEl).not.toHaveClass(StatisticDisplay.LOWER_WARNING_CLASS);
+      expect(valueEl).not.toHaveClass(StatisticDisplay.LOWER_LIMIT_CLASS);
+    });
+
+    test("should render statistics with an additional className when value is at threshold", () => {
+      const title = "A Title";
+      const startingValue = 7;
+      const upperWarning = 7;
+      const upperLimit = 19;
+
+      render(
+        <StatisticDisplay
+          title={title}
+          startingValue={startingValue}
+          upperWarning={upperWarning}
+          upperLimit={upperLimit}
+        />
+      );
+
+      expect(screen.getByText(title)).toBeInTheDocument();
+
+      const valueEl = screen.getByLabelText(title);
+      const statisticValue = valueEl.textContent;
+      expect(statisticValue).toBe(startingValue.toString());
+
+      expect(valueEl).not.toHaveClass(StatisticDisplay.UPPER_LIMIT_CLASS);
+      expect(valueEl).toHaveClass(StatisticDisplay.UPPER_WARNING_CLASS);
+      expect(valueEl).not.toHaveClass(StatisticDisplay.LOWER_WARNING_CLASS);
+      expect(valueEl).not.toHaveClass(StatisticDisplay.LOWER_LIMIT_CLASS);
+    });
+
+    test("should render statistics with an additional className when value is outside threshold", () => {
+      const title = "A Title";
+      const startingValue = 6;
+      const upperWarning = 7;
+      const upperLimit = 19;
+
+      render(
+        <StatisticDisplay
+          title={title}
+          startingValue={startingValue}
+          upperWarning={upperWarning}
+          upperLimit={upperLimit}
+        />
+      );
+
+      expect(screen.getByText(title)).toBeInTheDocument();
+
+      const valueEl = screen.getByLabelText(title);
+      const statisticValue = valueEl.textContent;
+      expect(statisticValue).toBe(startingValue.toString());
+
+      expect(valueEl).not.toHaveClass(StatisticDisplay.UPPER_LIMIT_CLASS);
+      expect(valueEl).not.toHaveClass(StatisticDisplay.UPPER_WARNING_CLASS);
+      expect(valueEl).not.toHaveClass(StatisticDisplay.LOWER_WARNING_CLASS);
+      expect(valueEl).not.toHaveClass(StatisticDisplay.LOWER_LIMIT_CLASS);
+    });
+  });
+
+  describe("Upper Limit threshold", () => {
+    test("should render statistics with an additional className when value is at threshold", () => {
+      const title = "A Title";
+      const startingValue = 19;
+      const upperLimit = 19;
+      const upperWarning = 7;
+
+      render(
+        <StatisticDisplay
+          title={title}
+          startingValue={startingValue}
+          upperLimit={upperLimit}
+          upperWarning={upperWarning}
+        />
+      );
+
+      expect(screen.getByText(title)).toBeInTheDocument();
+
+      const valueEl = screen.getByLabelText(title);
+      const statisticValue = valueEl.textContent;
+      expect(statisticValue).toBe(startingValue.toString());
+
+      expect(valueEl).toHaveClass(StatisticDisplay.UPPER_LIMIT_CLASS);
+      expect(valueEl).not.toHaveClass(StatisticDisplay.UPPER_WARNING_CLASS);
+      expect(valueEl).not.toHaveClass(StatisticDisplay.LOWER_WARNING_CLASS);
+      expect(valueEl).not.toHaveClass(StatisticDisplay.LOWER_LIMIT_CLASS);
+    });
+  });
 });
--- src/components/StatisticDisplay/StatisticDisplay.tsx
+++ src/components/StatisticDisplay/StatisticDisplay.tsx
@@ -7,18 +7,35 @@ import { sanitize } from "../../utils/lib/sanitizer";
 
 import "./index.css";
 
+const UPPER_LIMIT_CLASS = "statistic-display-upper-limit";
+const UPPER_WARNING_CLASS = "statistic-display-upper-warning";
 const LOWER_WARNING_CLASS = "statistic-display-lower-warning";
 const LOWER_LIMIT_CLASS = "statistic-display-lower-limit";
 
 interface StatisticDisplayProps {
   title: string;
   startingValue: number;
+  upperLimit: number;
+  upperWarning: number;
   lowerWarning: number;
   lowerLimit: number;
 }
 
 export default class StatisticDisplay extends React.Component<StatisticDisplayProps> {
-  static defaultProps = { lowerWarning: null, lowerLimit: null };
+  static defaultProps = {
+    upperLimit: null,
+    upperWarning: null,
+    lowerWarning: null,
+    lowerLimit: null,
+  };
+
+  static get UPPER_LIMIT_CLASS() {
+    return UPPER_LIMIT_CLASS;
+  }
+
+  static get UPPER_WARNING_CLASS() {
+    return UPPER_WARNING_CLASS;
+  }
 
   static get LOWER_WARNING_CLASS() {
     return LOWER_WARNING_CLASS;
@@ -36,26 +53,48 @@ export default class StatisticDisplay extends React.Component<StatisticDisplayPr
     this.id = uuidv4();
   }
 
-  isValueInLowerWarning() {
+  isValueAtUpperLimit() {
+    if (!this.props.upperLimit) return false;
+
+    return this.props.startingValue >= this.props.upperLimit;
+  }
+
+  isValueWithinUpperWarning() {
+    if (!this.props.upperWarning) return false;
+
+    return (
+      this.props.startingValue >= this.props.upperWarning &&
+      !this.isValueAtUpperLimit()
+    );
+  }
+
+  isValueAtLowerLimit() {
+    if (!this.props.lowerLimit) return false;
+
+    if (this.props.startingValue < this.props.lowerLimit)
+      throw new Error(
+        `The given value, '${this.props.startingValue}', is past the limit, '${this.props.lowerLimit}'.`
+      );
+
+    return this.props.startingValue == this.props.lowerLimit;
+  }
+
+  isValueWithinLowerWarning() {
     if (!this.props.lowerWarning) return false;
 
     return (
       this.props.startingValue <= this.props.lowerWarning &&
-      !(this.props.startingValue <= this.props.lowerLimit)
+      !this.isValueAtLowerLimit()
     );
-  }
-
-  isValueInLowerLimit() {
-    if (!this.props.lowerLimit) return false;
-
-    return this.props.startingValue <= this.props.lowerLimit;
   }
 
   render() {
     const labelID = sanitize(this.props.title) + "-" + this.id;
     const className = clsx({
-      [LOWER_WARNING_CLASS]: this.isValueInLowerWarning(),
-      [LOWER_LIMIT_CLASS]: this.isValueInLowerLimit(),
+      [UPPER_LIMIT_CLASS]: this.isValueAtUpperLimit(),
+      [UPPER_WARNING_CLASS]: this.isValueWithinUpperWarning(),
+      [LOWER_WARNING_CLASS]: this.isValueWithinLowerWarning(),
+      [LOWER_LIMIT_CLASS]: this.isValueAtLowerLimit(),
     });
 
     return (
