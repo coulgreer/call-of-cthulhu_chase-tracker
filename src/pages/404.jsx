6bd846aaa46bc3f3c7631f27319d38586234a3f9
cagreer18 <coulgreer1@hotmail.com>
2021-02-24 05:16:17 -0500

    Added a 404 page.

---
+++ src/pages/404.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SEO from "../components/SEO";
+
+import Layout from "../components/Layout";
+
+export default function Custom404() {
+  return (
+    <Layout>
+      <SEO />
+      <h2>Page not found</h2>
+      <p>Some things are meant to be forgotten...</p>
+    </Layout>
+  );
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import SEO from "../components/SEO";
 
 import Layout from "../components/Layout";
